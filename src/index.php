3b43df530e1e952d94dd787aa0696d86fc6f30d4
Gustavo <123191793+Gus41@users.noreply.github.com>
2026-04-09 23:50:42 +0000

    feat: taillwind nad partials setup

--- src/index.php
+++ src/index.php
@@ -1,1 +1,12 @@
-teste
+<?php
+    include 'partials/header.php';
+?>
+
+<div class="container mx-auto p-4 bg-red-100">
+    <h1 class="text-2xl font-bold mb-4">Hello world</h1>
+</div>
+ 
+
+<?php
+    include 'partials/footer.php';
+?>
---
+++ src/partials/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <title><?= $title ?? '' ?></title>
+  <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+</head>
+<body>
